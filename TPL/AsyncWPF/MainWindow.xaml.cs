98dda89df19ee28435e8e7b6b512874042857d7c
Aghasi Lorsabyan <aghasi.lorsabyan@mic.am>
2016-05-21 19:29:21 +0400

    TPL Solution added.

---
+++ TPL/AsyncWPF/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Windows;
+
+namespace AsyncWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void buttonGetHtml_Click(object sender, RoutedEventArgs e)
+        {
+            using (var client = new HttpClient())
+            {
+                var url = new Uri(textBoxUrl.Text);
+
+                textBoxHtml.Text = "Please wait...";
+
+                var html = await client.GetStringAsync(url);
+
+                textBoxHtml.Text = html;
+            }
+        }
+    }
+}
