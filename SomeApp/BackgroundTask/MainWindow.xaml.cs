41aaa041385e2e239897bae7bcbe18a5f7e8a3e2
Aghasi Lorsabyan <aghasi.lorsabyan@mic.am>
2016-05-24 19:07:20 +0400

    Updates.

---
+++ SomeApp/BackgroundTask/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BackgroundTask
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BackgroundWorker worker = new BackgroundWorker();
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            progress.Minimum = 1;
+            progress.Maximum = 100;
+
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
+
+            worker.DoWork += Worker_DoWork;
+            worker.RunWorkerCompleted += Worker_RunWorkerCompleted;
+            worker.ProgressChanged += Worker_ProgressChanged;
+
+
+        }
+
+        private void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progress.Value = e.ProgressPercentage;
+        }
+
+        private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                MessageBox.Show("Cancelled");
+                progress.Value = 1;
+            }
+            else
+            {
+                MessageBox.Show("Done");
+            }
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (worker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+
+                Thread.Sleep(100);
+                worker.ReportProgress(i);
+            }
+        }
+
+        private void buttonStart_Click(object sender, RoutedEventArgs e)
+        {
+            worker.RunWorkerAsync();
+        }
+
+        private void buttonStop_Click(object sender, RoutedEventArgs e)
+        {
+            worker.CancelAsync();
+        }
+    }
+}
---
+++ SomeApp/SomeApp/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SomeApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void buttonLoad_Click(object sender, RoutedEventArgs e)
+        {
+            buttonLoad.IsEnabled = false;
+            var input = textBoxInput.Text;
+            
+                textBoxOutput.Text = input;
+            
+            var slowTask = Task<string>.Factory.StartNew(() => SlowFunction(input));
+            textBoxOutput.Text += "\nDoing other things while waiting for that slow function...\r\n";
+            await slowTask;
+            textBoxOutput.Text += slowTask.Result;
+
+            buttonLoad.IsEnabled = true;
+        }
+        public string SlowFunction(string some)
+        {
+            Thread.Sleep(5000);
+            return some;
+        }
+    }
+}
