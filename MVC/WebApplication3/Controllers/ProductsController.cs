49d6600b070ba6b4f9f60d7c94b76d4c6abb6e64
Aghasi Lorsabyan <aghasi.lorsabyan@mic.am>
2016-06-11 19:04:46 +0400

    Added WebApplication2 and 3

---
+++ MVC/WebApplication3/Controllers/ProductsController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    [Authorize]
+    public class ProductsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        [AllowAnonymous]
+        // GET: Products
+        public async Task<ActionResult> Index(string order = "desc")
+        {
+            var products = await db.Products.ToListAsync();
+
+            if (order == "asc")
+            {
+                products = products.OrderBy(p => p.Price).ToList();
+            }
+
+            if (order == "desc")
+            {
+                products = products.OrderByDescending(p => p.Price).ToList();
+            }
+
+            return View(products);
+        }
+
+        [AllowAnonymous]
+        // GET: Products/Details/5
+        public async Task<ActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = await db.Products.FindAsync(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Name,Price")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                product.Id = Guid.NewGuid().ToString();
+                db.Products.Add(product);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        public async Task<ActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = await db.Products.FindAsync(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name,Price")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        public async Task<ActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = await db.Products.FindAsync(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(string id)
+        {
+            Product product = await db.Products.FindAsync(id);
+            db.Products.Remove(product);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+
+        public async Task<ActionResult> Random(int quantity)
+        {
+            if(quantity < 0)
+            {
+                Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                return Content("Quantity should be positive");
+            }
+
+            for (int i = 1; i <= quantity; i++)
+            {
+                db.Products.Add(new Product {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = $"Product {i}",
+                    Price = i * 10
+                });
+            }
+
+            await db.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MVC/WebApplication3/Models/ProductModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class Product
+    {
+        [ScaffoldColumn(false)]
+        public string Id { get; set; }
+        [Required]
+        [MinLength(3, ErrorMessage = "Invalid Name")]
+        [Display(Name = "Product Name")]
+        public string Name { get; set; }
+        [Required]
+        [Range(10.0, 1000000.0, ErrorMessage = "Invalid Price")]
+        [Display(Name = "Product Price in AMD")]
+        public double Price { get; set; }
+    }
+}
