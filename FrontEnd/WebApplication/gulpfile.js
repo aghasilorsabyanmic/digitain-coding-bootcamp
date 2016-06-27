8a83026a850eede2903e27e6c75af46ce721d268
Aghasi Lorsabyan <aghasi.lorsabyan@mic.am>
2016-06-27 11:00:32 +0400

    Added Gulp example

---
+++ FrontEnd/WebApplication/gulpfile.js
@@ -0,0 +1,20 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var less = require('gulp-less');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('default', function () {
+    return gulp.src('style.less')
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('style.less', ['default'])
+});
